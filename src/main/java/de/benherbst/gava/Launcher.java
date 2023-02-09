f5710fcadd62163a1b1a9073ad8815fdfdae87b2
Ben Herbst <ben.hbst@gmail.com>
2023-02-10 00:54:34 +0100

    Basic UI

---
+++ src/main/java/de/benherbst/gava/Launcher.java
@@ -0,0 +1,7 @@
+package de.benherbst.gava;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Main.main(args);
+    }
+}
--- src/main/java/de/benherbst/gava/Main.java
+++ src/main/java/de/benherbst/gava/Main.java
@@ -4,21 +4,29 @@ import de.benherbst.gava.compiler.CompilerInfo;
 import de.benherbst.gava.compiler.JavaFileCompiler;
 
 import java.io.File;
+import java.util.Objects;
 import java.util.Scanner;
 
-public final class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import static javafx.application.Application.launch;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getClassLoader().getResource("Main.fxml")));
+        primaryStage.setTitle("Gava - German with Java");
+        primaryStage.setScene(new Scene(root, 500, 400));
+        primaryStage.setResizable(false);
+        primaryStage.show();
+    }
+
     public static void main(String[] args) {
-        System.out.println("Path to .java file:");
-        Scanner scanner = new Scanner(System.in);
-        String path = scanner.nextLine();
-        File file = new File(path);
-
-        JavaFileCompiler javaFileCompiler = new JavaFileCompiler();
-
-        CompilerInfo info = javaFileCompiler.compile(file);
-
-        if (info == null) return;
-
-        javaFileCompiler.run(info);
+        launch(args);
     }
 }
