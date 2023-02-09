d1a6ea2846986fd1756e327fa95ade90e41b4dc4
Ben Herbst <ben.hbst@gmail.com>
2023-02-10 00:59:23 +0100

    Added Controller Class

---
+++ src/main/java/de/benherbst/gava/Controller.java
@@ -0,0 +1,25 @@
+package de.benherbst.gava;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class Controller {
+
+    @FXML
+    private Button chooseButton;
+
+    @FXML
+    private Button compileButton;
+
+    @FXML
+    void chooseFile(ActionEvent event) {
+
+    }
+
+    @FXML
+    void compile(ActionEvent event) {
+
+    }
+
+}
